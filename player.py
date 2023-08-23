e0eb244269c03aee7af55900f01f0c22f71fa1b4
nvkaa <80764308+nvkaa@users.noreply.github.com>
2023-08-24 00:44:34 +0700

    Add files via upload

--- player.py
+++ player.py
@@ -20,8 +20,8 @@ hd = pygame.image.load(os.path.join('Graphics', 'snake', 'head','head_down.png')
 bh = pygame.image.load(os.path.join('Graphics', 'snake', 'body','body_horizontal.png'))
 bv = pygame.image.load(os.path.join('Graphics', 'snake', 'body','body_vertical.png'))
 btl = pygame.image.load(os.path.join('Graphics', 'snake', 'body','body_tl.png'))
-btl = pygame.image.load(os.path.join('Graphics', 'snake', 'body','body_tl.png'))
-btl = pygame.image.load(os.path.join('Graphics', 'snake', 'body','body_tl.png'))
+btr = pygame.image.load(os.path.join('Graphics', 'snake', 'body','body_tr.png'))
+bbr = pygame.image.load(os.path.join('Graphics', 'snake', 'body','body_br.png'))
 bbl = pygame.image.load(os.path.join('Graphics', 'snake', 'body','body_bl.png'))
 
 # 
@@ -51,17 +51,15 @@ class Player(pygame.sprite.Sprite):
         self.randomize_head() 
 
     def randomize_dir(self):
-        dir = ['r', 'l', 'u', 'd']
-        indx = random.randint(0, len(dir)-1)
-        self.dir = dir[indx]
+        # dir = ['r', 'l', 'u', 'd']
+        # indx = random.randint(0, len(dir)-1)
+        # self.dir = dir[indx]
+        self.dir = 'r'
      
     
-    def randomize_head(self):
-        # self.head_x = random.randint(0,WIDTH - 90) + self.speedX
-        # self.head_y = random.randint(0,HEIGHT - 90) + self.speedY
-        
-        self.head_x = random.randint(0,WIDTH - 90)
-        self.head_y = random.randint(0,HEIGHT - 90) 
+    def randomize_head(self):        
+        self.head_x = random.randint(90,WIDTH - 90)
+        self.head_y = random.randint(90,HEIGHT - 90) 
         self.head_w, self.head_h = BLOCK_SIZE, BLOCK_SIZE
         self.head_rect  = pygame.Rect(self.head_x, self.head_y, self.head_w, self.head_h) 
 
@@ -90,16 +88,22 @@ class Player(pygame.sprite.Sprite):
             self.tail_x = self.head_x 
             self.tail_y = self.head_y - 2*BLOCK_SIZE
 
+        # headx, heady = self.head_x, self.head_y
+
+        bodyx, bodyy = self.body_x, self.body_y
+
+        tailx, taily = self.tail_x, self.tail_y
+
         self.snake = [
             [
-                self.tail_x, 
-                self.tail_y,
+                tailx, 
+                taily,
                 "tail", 
                 self.dir,
                 self.tail_img,
             ], [
-                self.body_x, 
-                self.body_y,
+                bodyx, 
+                bodyy,
                 "body", 
                 self.dir,
                 self.body_img
@@ -120,7 +124,7 @@ class Player(pygame.sprite.Sprite):
     def drawSelf(self, screen):
         # pygame.Surface.blit(screen, self.head_img, self.head_rect )
         # pygame.draw.rect(screen, self.color, self.re ct)
-
+        
         sprite_list = []
         for part in self.snake:
             if part[2] == "head":
@@ -143,7 +147,7 @@ class Player(pygame.sprite.Sprite):
                     self.head_img = hr
                     part[4] = hr
                     sprite_list.append(part)
-
+                
             if part[2] == "body":
                 if part[3] == "u":
                     self.body_img = bv
@@ -186,51 +190,152 @@ class Player(pygame.sprite.Sprite):
                     part[4] = tl
                     sprite_list.append(part)
 
-            part[0] += self.speedX
-            part[1] += self.speedY
+
+            
+
+
 
         for part in sprite_list:
             pygame.Surface.blit(screen, part[4], (part[0], part[1]))
 
     def move(self, keys):
-        turning_point = []
+        tp = self.snake
         for part in self.snake:
-            turning_point.insert(
-                -1, 
-                [part[0], part[1], part[3]]
-            )
-        print(turning_point)
+            if keys == pygame.K_UP and part[3] != 'd':
+                self.snake[-1][3] = 'u'
+                self.speedY = 10
+                self.speedX = 0
+                self.snake[-1][1] -= self.speedY
 
-        if keys == pygame.K_UP and self.dir != 'd':
-            self.dir = 'u'
-            self.change_dir(turning_point) 
-            self.speedY = -3
-            self.speedX = 0
+                if part[3] == 'u':
+                    for block in self.snake[:-1]:
+                        self.speedY = 10
+                        self.speedX = 0
+                        block[1] -= self.speedY
+                    
+                elif part[3] == 'r':
+                    for block in self.snake[:-1]:
+                        if block[0] < tp[-1][0]:
+                            self.snake[-2][4] = btl
+                            self.speedX = 10
+                            block[0] += self.speedX
+                        else: 
+                            self.speedX = 0
+                            self.speedY = 10
+                            block[1] -= self.speedY
+                            block[3] = 'u'
+                elif part[3] == 'l':
+                    if block[0] > tp[-1][0]:
+                        self.snake[-2][4] = btr
+                        self.speedX = 10
+                        block[0] -= self.speedX
+                    else: 
+                        self.speedX = 0
+                        self.speedY = 10
+                        block[1] -= self.speedY
+                        block[3] = 'u'
+            
+            elif keys == pygame.K_DOWN and part[3] != 'u':
+                self.snake[-1][3] = 'd'
+                self.speedY = 10
+                self.speedX = 0
+                self.snake[-1][1] += self.speedY
 
-        elif keys == pygame.K_DOWN and self.dir != 'u':
-            self.dir = 'd'
-            self.change_dir(turning_point)
-            self.speedY = 3
-            self.speedX = 0
+                if part[3] == 'd':
+                    for block in self.snake[:-1]:
+                        self.speedY = 10
+                        self.speedX = 0
+                        block[1] += self.speedY
+                    
+                elif part[3] == 'r':
+                    for block in self.snake[:-1]:
+                        if block[0] < tp[-1][0]:
+                            self.snake[-2][4] = bbl
+                            self.speedX = 10
+                            block[0] += self.speedX
+                        else: 
+                            self.speedX = 0
+                            self.speedY = 10
+                            block[1] += self.speedY
+                            block[3] = 'd'
+                
+                elif part[3] == 'l':
+                    if block[0] > tp[-1][0]:
+                        self.snake[-2][4] = bbl
+                        self.speedX = 10
+                        block[0] -= self.speedX
+                    else: 
+                        self.speedX = 0
+                        self.speedY = 10
+                        block[1] += self.speedY
+                        block[3] = 'u'
 
-        elif keys == pygame.K_LEFT and self.dir != 'r':
-            self.dir = 'l'
-            self.change_dir(turning_point)
-            self.speedY = 0
-            self.speedX = -3
+            if keys == pygame.K_LEFT and part[3] != 'r':
+                self.snake[-1][3] = 'l'
+                self.speedY = 0
+                self.speedX = 10
+                self.snake[-1][0] -= self.speedX
 
-        elif keys == pygame.K_RIGHT and self.dir != 'l':
-            self.dir = 'r'
-            self.change_dir(turning_point)
-            self.speedY = 0
-            self.speedX = 3
+                if part[3] == 'l':
+                    for block in self.snake[:-1]:
+                        self.speedY = 0
+                        self.speedX = 10
+                        block[0] -= self.speedX
+                    
+                elif part[3] == 'u':
+                    for block in self.snake[:-1]:
+                        if block[1] > tp[-1][1]:
+                            self.snake[-2][4] = bbl
+                            self.speedY = 10
+                            block[1] -= self.speedY
+                        else: 
+                            self.speedX = 10
+                            self.speedY = 0
+                            block[0] -= self.speedX
+                            block[3] = 'l'
+
+                elif part[3] == 'd':
+                    if block[1] < tp[-1][1]:
+                        self.snake[-2][4] = btl
+                        self.speedX = 10
+                        block[0] -= self.speedX
+                    else: 
+                        self.speedX = 10
+                        self.speedY = 0
+                        block[0] -= self.speedX
+                        block[3] = 'l'
+
+            if keys == pygame.K_RIGHT and part[3] != 'l':
+                self.snake[-1][3] = 'r'
+                self.speedY = 0
+                self.speedX = 10
+                self.snake[-1][0] += self.speedX
+
+                if part[3] == 'r':
+                    for block in self.snake[:-1]:
+                        self.speedY = 0
+                        self.speedX = 10
+                        block[0] += self.speedX
+                    
+                elif part[3] == 'u':
+                    for block in self.snake[:-1]:
+                        if block[1] > tp[-1][1]:
+                            self.snake[-2][4] = bbr
+                            self.speedY = 10
+                            block[0] -= self.speedY
+                        else: 
+                            self.speedX = 10
+                            self.speedY = 0
+                            block[1] += self.speedX
+                            block[3] = 'r'
+                        
 
 
 
     def change_dir(self, t_p):
         for part in t_p:
             if part[2] == 'r' and self.dir == 'u':
-                
+                pass
 
         # if self.dir == 'r':
         #     self.head_img = hr
